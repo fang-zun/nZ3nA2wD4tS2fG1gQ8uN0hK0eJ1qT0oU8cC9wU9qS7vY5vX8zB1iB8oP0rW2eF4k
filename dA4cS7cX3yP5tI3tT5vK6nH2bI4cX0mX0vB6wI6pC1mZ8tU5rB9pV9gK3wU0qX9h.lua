2d88d891471a08b5080b42d49348ce9b17817f32
fuck you titan td <newone114609@gmail.com>
2026-04-20 15:23:41 +0700

    Update dA4cS7cX3yP5tI3tT5vK6nH2bI4cX0mX0vB6wI6pC1mZ8tU5rB9pV9gK3wU0qX9h.lua

--- dA4cS7cX3yP5tI3tT5vK6nH2bI4cX0mX0vB6wI6pC1mZ8tU5rB9pV9gK3wU0qX9h.lua
+++ dA4cS7cX3yP5tI3tT5vK6nH2bI4cX0mX0vB6wI6pC1mZ8tU5rB9pV9gK3wU0qX9h.lua
@@ -1,3 +1,14 @@
 return true, {
   "cJ<7hPc.:,{4g:]XY",
+  ".Sle<qWY;}Er=mo",
+  "Ic)MU632hN:eHuNq", 
+  "R50AHjWov,x4qe8Kb",
+  "A,yivV!W=hY+n]N",
+  "QIEv<tCv#dtX,0x:", 
+  "e%.e?6#C%.TU&u&]", 
+  "dWPvFnHJ%,y38O&{S",
+  "iz;h]@Oosy37k-Pr", 
+  "afY[@[%,u$(IgPMH,",
+  "Br=Of0YLETkaXo^8",
+  "&U{;jD!ln#>F=){",
 }
